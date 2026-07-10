3a53e3f7b359e6190ebf22d32ad1ecd204c198d8
sudeshnarayana <sudeshnarayana2001@gmail.com>
2026-07-11 02:37:27 +0530

    Add build script and fix supabase env vars

--- frontend/src/supabaseClient.ts
+++ frontend/src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://rofgxbkuoyfhdnobigrb.supabase.co'
-const supabaseAnonKey = 'sb_publishable_KL4v-2rP_7p71gp26hkfXQ_pCxmN4VL'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvZmd4Ymt1b3lmaGRub2JpZ3JiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNzAzOTcsImV4cCI6MjA5ODY0NjM5N30.JzCbAgM6lEiMCYtpipF2OLK9vbdWRnGEx9IDUTXIbFw'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
